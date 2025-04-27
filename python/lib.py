afb1b31578d768a3e13d7a7f3863ab5581f4ae73
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-04-27 20:29:18 +0900

    Python 言語によるハフマン符号を実装

---
+++ python/lib.py
@@ -0,0 +1,48 @@
+import heapq
+
+def get_input():
+    print("ハフマン符号化したい文字列を英小文字か平仮名で入力してください: ", end="")
+    return input().strip()
+
+def get_huffman_code(counter):
+    heap = []
+    for key, value in counter.items():
+        heapq.heappush(heap, (value, key))
+
+    huffman_code = dict()
+    for key in counter.keys():
+        huffman_code[key] = []
+
+    while len(heap) > 1:
+        value1, key1 = heapq.heappop(heap)
+        value2, key2 = heapq.heappop(heap)
+        new_key = key1 + key2
+        new_value = value1 + value2
+        heapq.heappush(heap, (new_value, new_key))
+
+        for c in key1:
+            huffman_code[c].append("0")
+
+        for c in key2:
+            huffman_code[c].append("1")
+
+    for key in counter.keys():
+        huffman_code[key] = "".join(huffman_code[key])
+
+    return huffman_code
+
+def print_huffman_code(huffman_code):
+    print("---ハフマン符号の対応表---")
+    for key, value in huffman_code.items():
+        print(f"{key}: {value}")
+    print("--------------------------")
+
+def get_encoded_huffman_code(huffman_code, source):
+    encoded = []
+    for c in source:
+        encoded.append(huffman_code[c])
+    return "".join(encoded)
+
+def print_source_and_encoded_huffman_code(source, encoded_huffman_code):
+    print("ハフマン符号化する前の文字列:", source)
+    print("ハフマン符号化した後の文字列:", encoded_huffman_code)
